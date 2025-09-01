dd1f027e70a7c95cc083d44c42474f654d98511f
KatiaKalynchuk <katerynakalynchuk@gmail.com>
2025-09-01 17:32:52 +0300

    lesson 12 - ACID lab

--- node-js-lessons/lesson-12-acid-lab/acid-lab/src/transfer/transfer.controller.ts
+++ node-js-lessons/lesson-12-acid-lab/acid-lab/src/transfer/transfer.controller.ts
@@ -1,31 +1,13 @@
-// // transfer.controller.ts
-// import { Controller, Post, Body } from '@nestjs/common';
-// import { TransferService } from './transfer.service';
-//
-// @Controller('transfer')
-// export class TransferController {
-//   constructor(private transferService: TransferService) {}
-//
-//   @Post()
-//   async create(@Body() body: { fromId: string; toId: string; amount: number }) {
-//     const movement = await this.transferService.transfer(
-//       body.fromId,
-//       body.toId,
-//       body.amount,
-//     );
-//     return movement;
-//   }
-// }
-
 import { Controller, Post, Body } from '@nestjs/common';
 import { TransferService } from './transfer.service';
+import { CreateTransferDto } from './dto/CreateTransferDto';
 
 @Controller('transfer')
 export class TransferController {
   constructor(private readonly transferService: TransferService) {}
 
   @Post()
-  async create(@Body() body: { fromId: string; toId: string; amount: number }) {
-    return this.transferService.transfer(body.fromId, body.toId, body.amount);
+  async create(@Body() body: CreateTransferDto) {
+    return this.transferService.transfer(body);
   }
 }
--- node-js-lessons/lesson-12-acid-lab/acid-lab/src/transfer/transfer.service.ts
+++ node-js-lessons/lesson-12-acid-lab/acid-lab/src/transfer/transfer.service.ts
@@ -1,33 +1,49 @@
 import { Injectable, BadRequestException } from '@nestjs/common';
 import { DataSource } from 'typeorm';
-import { Account } from '../account/entities/account.entity';
-import { Movement } from '../movements/entities/movements.entity';
+import { Account } from '../entities/account.entity';
+import { Movement } from '../entities/movement.entity';
+import { CreateTransferDto } from './dto/CreateTransferDto';
 
 @Injectable()
 export class TransferService {
   constructor(private dataSource: DataSource) {}
 
-  async transfer(fromId: string, toId: string, amount: number) {
-    return this.dataSource.transaction(async (manager) => {
-      const from = await manager.findOne(Account, { where: { id: fromId } });
-      const to = await manager.findOne(Account, { where: { id: toId } });
+  async transfer(dto: CreateTransferDto) {
+    const { from_id, to_id, amount } = dto;
 
-      if (!from || !to) {
+    return this.dataSource.transaction(async (manager) => {
+      const fromAccount = await manager.findOne(Account, {
+        where: { id: from_id },
+        lock: { mode: 'pessimistic_write' },
+      });
+      const toAccount = await manager.findOne(Account, {
+        where: { id: to_id },
+        lock: { mode: 'pessimistic_write' },
+      });
+
+      if (!fromAccount || !toAccount) {
         throw new BadRequestException('Account not found');
       }
 
-      if (Number(from.balance) < amount) {
+      if (+fromAccount.balance < +amount) {
         throw new BadRequestException('Insufficient funds');
       }
 
-      from.balance = Number(from.balance) - amount;
-      to.balance = Number(to.balance) + amount;
+      const fromBalance = Number(fromAccount.balance) - Number(amount);
+      const toBalance = Number(toAccount.balance) + Number(amount);
 
-      await manager.save(from);
-      await manager.save(to);
+      fromAccount.balance = fromBalance.toFixed(2);
+      toAccount.balance = toBalance.toFixed(2);
 
-      const movement = manager.create(Movement, { from, to, amount });
-      return manager.save(movement);
+      await manager.save(fromAccount);
+      await manager.save(toAccount);
+
+      const transfer = manager.create(Movement, {
+        fromAccount,
+        toAccount,
+        amount,
+      });
+      return manager.save(transfer);
     });
   }
 }
